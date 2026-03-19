0b5d4b0c7d29f9d4ab6b39c1bb58a16835562976
PeachCoolpis <rlglxh@naver.com>
2026-03-19 20:30:16 +0900

    feat. 앱, 백엔드 프로젝트 세팅

---
+++ chamApi/src/main/java/com/chamapi/ChamApiApplication.java
@@ -0,0 +1,13 @@
+package com.chamapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChamApiApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(ChamApiApplication.class, args);
+    }
+    
+}
---
+++ chamApi/src/test/java/com/chamapi/ChamApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chamapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChamApiApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
