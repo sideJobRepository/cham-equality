da0dc47495c71f25059f826ad26885bb5c9cb5b4
PeachCoolpis <rlglxh@naver.com>
2026-03-30 22:09:07 +0900

    feat. PageResponse 구현

---
+++ chamApi/src/main/java/com/chamapi/page/PageResponse.java
@@ -0,0 +1,26 @@
+package com.chamapi.page;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public record PageResponse<T>(List<T> content, int page, int size, long totalElements, int totalPages, boolean first,
+                              boolean last, boolean empty,   Long todaySignupCount ) {
+    
+    public static <T> PageResponse<T> from(Page<T> p) {
+        return new PageResponse<>(
+                p.getContent(),
+                p.getNumber(),
+                p.getSize(),
+                p.getTotalElements(),
+                p.getTotalPages(),
+                p.isFirst(),
+                p.isLast(),
+                p.isEmpty(),
+                null
+        );
+    }
+}
