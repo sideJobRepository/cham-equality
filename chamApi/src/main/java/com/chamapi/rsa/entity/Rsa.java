d69ef3767ef0c75ee0c53bbbd5b6a8246d7e1218
PeachCoolpis <rlglxh@naver.com>
2026-03-30 22:48:40 +0900

    feat. Rsa 엔티티 생성(키 새로 발급받아서 테이블에 넣어둠)

---
+++ chamApi/src/main/java/com/chamapi/rsa/entity/Rsa.java
@@ -0,0 +1,25 @@
+package com.chamapi.rsa.entity;
+
+import com.chamapi.mapperd.DateSuperClass;
+import jakarta.persistence.*;
+import lombok.*;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Table(name = "RSA")
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Rsa extends DateSuperClass {
+    
+    
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "RSA_ID")
+    private Long id;
+    
+    @Column(name = "RSA_PRIVATE_KEY")
+    private String rsaPrivateKey;
+}
---
+++ chamApi/src/main/java/com/chamapi/rsa/repository/RsaRepository.java
@@ -0,0 +1,8 @@
+package com.chamapi.rsa.repository;
+
+import com.chamapi.rsa.entity.Rsa;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RsaRepository extends JpaRepository<Rsa, Long> {
+
+}
