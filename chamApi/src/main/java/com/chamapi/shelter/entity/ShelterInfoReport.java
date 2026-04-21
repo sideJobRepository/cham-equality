eb95d8087bb82e9c5c0bd08977b50b18ec4dbd31
PeachCoolpis <rlglxh@naver.com>
2026-04-21 22:42:07 +0900

    refactor. 엔티티 변경

--- chamApi/src/main/java/com/chamapi/shelter/entity/ShelterInfoReport.java
+++ chamApi/src/main/java/com/chamapi/shelter/entity/ShelterInfoReport.java
@@ -7,44 +7,46 @@ import lombok.*;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
-@Table(name = "SHELTER_INFO_REGISTER_REQUEST")
+
+@Table(name = "SHELTER_INFO_REPORT")
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
-public class ShelterInfoRegisterRequest extends DateSuperClass {
-
+public class ShelterInfoReport extends DateSuperClass {
+    
+    
     // 대피소 정보 등록 요청 ID
     @Id
-    @Column(name = "SHELTER_INFO_REGISTER_REQUEST_ID")
+    @Column(name = "SHELTER_INFO_REPORT_ID")
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
-
+    
     // 대피소 ID
     @Column(name = "SHELTER_ID")
     private Long shelterId;
-
+    
     // 현장 확인 시설명
     @Column(name = "SHELTER_NAME")
     private String name;
-
+    
     // 건축 연도
     @Column(name = "SHELTER_BUILT_YEAR")
     private Integer builtYear;
-
+    
     // 안전 등급(내진설계)
     @Column(name = "SHELTER_SAFETY_GRADE")
     private Integer safetyGrade;
-
+    
     // 장애인 화장실 여부
     @Column(name = "SHELTER_ACCESSIBLE_TOILET_WHETHER")
     private Boolean accessibleToiletWhether;
-
+    
     // 경사로 여부
     @Column(name = "SHELTER_RAMP_WHETHER")
     private Boolean rampWhether;
-
+    
     // 엘리베이터 여부
     @Column(name = "SHELTER_ELEVATOR_WHETHER")
     private Boolean elevatorWhether;
@@ -52,19 +54,19 @@ public class ShelterInfoRegisterRequest extends DateSuperClass {
     // 대피소 기타 접근성 시설
     @Column(name = "SHELTER_ETC_ACCESSIBILITY_FACILITIES")
     private String etcAccessibilityFacilities;
-
+    
     // 점자 블록 여부
     @Column(name = "SHELTER_BRAILLE_BLOCK_WHETHER")
     private Boolean brailleBlockWhether;
-
+    
     // 안내문 언어
     @Column(name = "SHELTER_SIGNAGE_LANGUAGE")
     private String signageLanguage;
-
+    
     // 조사자 메모
     @Column(name = "REQUEST_NOTE", length = 4000)
     private String requestNote;
-
+    
     // 요청 상태
     @Enumerated(EnumType.STRING)
     @Column(name = "REQUEST_STATUS")
