b380d0ab9c4f4748dd56a10746b9b97b10abd2a5
sj <slaayer60@gmail.com>
2026-07-30 00:03:30 +0900

    feat: 일일재난안전레포트 저장시 db 기준 시간으로 저장하도록 변경

--- crawling/src/app/pipeline/store.py
+++ crawling/src/app/pipeline/store.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from sqlalchemy import func
 
 from app.core.db import daily_report, engine
 from app.pipeline.schemas import SummaryResult
@@ -6,16 +6,15 @@ from app.pipeline.schemas import SummaryResult
 
 def save(summary_result: SummaryResult) -> None:
     crawl_result = summary_result.refine_result.crawl_result
-    now = datetime.now()
 
     stmt = daily_report.insert().values(
         ORIGIN_TITLE=crawl_result.title,
         ORIGIN_URL=crawl_result.origin_url,
         REFINED_HTML=summary_result.refine_result.refined_html,
         SUMMARY=summary_result.summary,
-        CREATE_DATE=now,
-        MODIFY_DATE=now,
+        CREATE_DATE=func.now(),
+        MODIFY_DATE=func.now(),
     )
 
-    with engine.begin() as conn:
+    with engine.begin() as conn:    
         conn.execute(stmt)
